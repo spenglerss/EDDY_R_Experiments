a04a18819dc329c036fbdc0c7509d41f195ed226
Sandesh Sadalge <ssadalge@educationdynamics.com>
2019-02-01 16:53:40 -0500

    commit

--- GET POST Experiments.R
+++ GET POST Experiments.R
@@ -3,19 +3,52 @@ library(jsonlite)
 
 # Experiments to get results from EDDY Matching Engine as a Proof Of Concept for the ability to analyze the information in real-time
 
-url = 'http://drupal-issvc.eddyprod.local/EDDY.IS.MatchingEngine.Service/MatchingService.svc/json/GetInstitutions'
-body_stuff <- fromJSON('{"directoryMatchRequest":{"TrackGuid":"351C687F-E7A2-42F8-A3AA-31B4D4C792F2","MaxResultsCount":5,"ProspectInput":{"FirstName":"san","LastName":"sad","StateId":32,"CountryId":4,"PostalCode":"07070","IsCitizen":"true"},"SortMethod":1}}')
+GetInstitutions_url = 'http://drupal-issvc.eddyprod.local/EDDY.IS.MatchingEngine.Service/MatchingService.svc/json/GetInstitutions'
+GetSintitutions_body1 <- list(
+  "directoryMatchRequest" = list(
+    "TrackGuid" = "351C687F-E7A2-42F8-A3AA-31B4D4C792F2",
+    "MaxResultsCount" = 5,
+    "MaxNestedProgramCount" = 2,
+    "ProspectInput" = list(
+      "FirstName" = "san",
+      "LastName" = "sad",
+      "StateId" = 32,
+      "CountryId" = 4,
+      "PostalCode" = "07070",
+      "IsCitizen" = "true"
+    ),
+    "SortMethod" = 1
+  )
+) 
 
+GetSintitutions_body2 <- list(
+  "directoryMatchRequest" = list(
+    "TrackGuid" = "351C687F-E7A2-42F8-A3AA-31B4D4C792F2",
+    "MaxResultsCount" = 5,
+    #"MaxNestedProgramCount" = 2,
+    "ProspectInput" = list(
+      "FirstName" = "san",
+      "LastName" = "sad",
+      "StateId" = 32,
+      "CountryId" = 4,
+      "PostalCode" = "07070",
+      "IsCitizen" = "true"
+    ),
+    "SortMethod" = 1
+  )
+) 
 
-r <- POST(url = url, body = body_stuff, encode = "json", verbose())
+r1 <- POST(url = GetInstitutions_url, body = GetSintitutions_body1, encode = "json")
+r2 <- POST(url = GetInstitutions_url, body = GetSintitutions_body2, encode = "json")
 
-r
-warn_for_status(r)
-r$content
+warn_for_status(r1)
+warn_for_status(r2)
+#r$content
 
- content(r, "text")
+# content(r, "text")
 
-results <- fromJSON(content(r, "text"))
+results1 <- fromJSON(content(r1, "text"))
+results2 <- fromJSON(content(r2, "text"))
 
 
 
