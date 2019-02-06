421e24239e4c70d705e3cb3c53f0e48e9e1524c7
Sandesh Sadalge <ssadalge@educationdynamics.com>
2019-02-06 17:09:25 -0500

    c

--- Wizard Match Call and Results Experiments.R
+++ Wizard Match Call and Results Experiments.R
@@ -4,77 +4,19 @@ library(odbc)
 
 # Experiments to get results from EDDY Matching Engine as a Proof Of Concept for the ability to analyze the information in real-time
 
-GetInstitutions_url = 'http://drupal-issvc.eddyprod.local/EDDY.IS.MatchingEngine.Service/MatchingService.svc/json/GetInstitutions'
 GetWizard_url = 'http://drupal-issvc.eddyprod.local/EDDY.IS.MatchingEngine.Service/MatchingService.svc/json/GetWizardMatches'
 
 con_reportcluster01 <- dbConnect(odbc(), 
-                Driver = "SQL Server", 
-                Server = "ReportCluster01", 
-                Database = "EDDYTracking", 
-                Trusted_Connection = "True")
+                                 Driver = "SQL Server", 
+                                 Server = "ReportCluster01", 
+                                 Database = "EDDYTracking", 
+                                 Trusted_Connection = "True")
 
 con_eddyprod <- dbConnect(odbc(), 
-                Driver = "SQL Server", 
-                Server = "isdb.eddyprod.local", 
-                Database = "EDDYTracking", 
-                Trusted_Connection = "True")
-
-
-
-
-
-GetIntitutions_body1 <- list(
-  "directoryMatchRequest" = list(
-    "TrackGuid" = "351C687F-E7A2-42F8-A3AA-31B4D4C792F2",
-    "MaxResultsCount" = 7,
-    "MaxNestedProgramCount" = 2,
-    "ProspectInput" = list(
-      "FirstName" = "san",
-      "LastName" = "sad",
-      "Email" = "blahblah123@test.com",
-      "StateId" = 32,
-      "CountryId" = 4,
-      "PostalCode" = "07070",
-      "IsCitizen" = "true"
-    ),
-    "SortMethod" = 1
-  )
-) 
-
-GetIntitutions_body2 <- list(
-  "directoryMatchRequest" = list(
-    "TrackGuid" = "351C687F-E7A2-42F8-A3AA-31B4D4C792F2",
-    "MaxResultsCount" = 7,
-    #"MaxNestedProgramCount" = 2,
-    "ProspectInput" = list(
-      "FirstName" = "san",
-      "LastName" = "sad",
-      "Email" = "blahblah123@test.com",
-      "StateId" = 32,
-      "CountryId" = 4,
-      "PostalCode" = "07070",
-      "IsCitizen" = "true"
-    ),
-    "SortMethod" = 1
-  )
-) 
-
-
-r1 <- POST(url = GetInstitutions_url, body = GetIntitutions_body1, encode = "json")
-r2 <- POST(url = GetInstitutions_url, body = GetIntitutions_body2, encode = "json")
-
-warn_for_status(r1)
-warn_for_status(r2)
-#r$content
-
-# content(r, "text")
-
-results1 <- fromJSON(content(r1, "text"))
-results2 <- fromJSON(content(r2, "text"))
-
-results1$GetInstitutionsResult$MatchResponseGuid
-
-
+                          Driver = "SQL Server", 
+                          Server = "isdb.eddyprod.local", 
+                          Database = "EDDYTracking", 
+                          Trusted_Connection = "True")
 
 
 # Creating a Unique Email to use so that the unique criterias in Matching Engine don't "dupe" it out:
@@ -87,11 +29,10 @@ unique_test_email <- paste("R_test_",
                            format(Sys.time(),"%SS"),
                            "@test.com",sep = "")
 
+
 GetWizard_body1 <- list(
   "wizardMatchRequest" = list(
     "TrackGuid" = "C665917F-32A9-4444-B5A9-8AC66F5D6E79",
-    # "MaxResultsCount" = 7,
-    # "MaxNestedProgramCount" = 2,
     "ProspectInput" = list(
       "Email" = unique_test_email,
       "PostalCode" = "11432"
@@ -103,17 +44,15 @@ GetWizard_body1 <- list(
 )
 
 
-w1 <- POST(url = GetWizard_url, body = GetWizard_body1, encode = "json")
-
-warn_for_status(w1)
-
 # First you need to call Matching Engine to get the Smart Matches then you have to call it again, but for school selection
+
+w1 <- POST(url = GetWizard_url, body = GetWizard_body1, encode = "json")
+warn_for_status(w1)
 wiz_result1 <- fromJSON(content(w1,"text"))
 
-wiz_result1$GetWizardMatchesResult$MatchResponseGuid
+# wiz_result1$GetWizardMatchesResult$MatchResponseGuid
 
-# For School Selection you need to modify the call to send a list with the institutionIds as exclusions:
-
+# For School Selection you need to modify 4 properties to get the School Selections:
 GetWizard_body2 <- GetWizard_body1
 GetWizard_body2$wizardMatchRequest$LeadCreationType <- 4
 GetWizard_body2$wizardMatchRequest$IncludeSmartMatchList <- "false"
@@ -121,26 +60,26 @@ GetWizard_body2$wizardMatchRequest$IncludeSchoolSelectionList <- "true"
 GetWizard_body2$wizardMatchRequest$SmartMatchedInstitutionIdList <- wiz_result1$GetWizardMatchesResult$SmartMatchList$InstitutionId
 
 w2 <- POST(url = GetWizard_url, body = GetWizard_body2, encode = "json")
-
 warn_for_status(w2)
-
 wiz_result2 <- fromJSON(content(w2,"text"))
 
 # It takes a few minutes for information to be saved to Porduction then replicated to ReportCluster01
 # The rest of this needs to be run some time (like 2-5 mins) afterwards
 
+
+#This SQL returns 2 JSONs 1st if the Removal Entities and the 2nd is the post request itself
 sql_for_removal <- paste("SELECT mrrrj.JsonObject AS RemovalJSON, mr.RequestInput FROM EddyTracking.dbo.MatchResponseRemovalReasonJson mrrrj (NOLOCK)
 INNER JOIN EddyTracking.dbo.MatchResponse mr (NOLOCK) ON mr.MatchResponseId = mrrrj.MatchResponseId
 WHERE mr.MatchResponseGUID = '",wiz_result1$GetWizardMatchesResult$MatchResponseGuid, "'", sep = "")
 
 
-sql_results_clust <- dbFetch(dbSendQuery(con_reportcluster01,sql_for_removal))
+sql_results_rptclust <- dbFetch(dbSendQuery(con_reportcluster01,sql_for_removal))
 # sql_results_prod <- dbFetch(dbSendQuery(con_eddyprod,sql_for_removal))
 
-removal_JSON_as_list_clust <- fromJSON(sql_results_clust$RemovalJSON)
+removal_JSON_as_list_rptclust <- fromJSON(sql_results_rptclust$RemovalJSON)
 #removal_JSON_as_list_prod <- fromJSON(sql_results_prod$RemovalJSON)
 
-requestinput_JSON_as_list_clust <- fromJSON(sql_results_clust$RequestInput)
+request_input_JSON_as_list_rptclust <- fromJSON(sql_results_rptclust$RequestInput)
 #requestinput_JSON_as_list_prod <- fromJSON(sql_results_prod$RequestInput)
 
 
